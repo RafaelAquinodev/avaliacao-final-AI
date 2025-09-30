3348755a87e6169acb90a533227fa7952e75d064
Erick Araújo <araujoerick.dev@gmail.com>
2025-09-30 20:51:17 -0300

    feat: update colors theme

--- src/app/questionario/_components/questionnaireHeader.tsx
+++ src/app/questionario/_components/questionnaireHeader.tsx
@@ -11,7 +11,7 @@ export function QuestionnaireHeader({
   progress,
 }: QuestionnaireHeaderProps) {
   return (
-    <div className="mb-8 fixed top-0 left-0 right-0 bg-background/90 backdrop-blur-sm z-10 py-4 px-6 md:px-[8%] border-b border-border">
+    <div className="mb-8 fixed top-0 left-0 right-0 bg-card backdrop-blur-sm z-10 py-4 px-6 md:px-[8%] border-b border-border">
       <div className="flex justify-between gap-4 items-center mb-4">
         <h1 className="text-2xl font-bold">Quiz de Background Profissional</h1>
         <span className="text-sm text-muted-foreground">
